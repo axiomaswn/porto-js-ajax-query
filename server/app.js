008620aff46452c4bc7aed794147a200c6582b55
YOMA <email": null>
2017-02-19 16:49:42 +0700

    menampilkan warna, tapi index.html belum diatur

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var color = require('./routes/color');
-
+const cors = require('cors');
 var app = express();
 
 // view engine setup
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', index);
 app.use('/color', color);
 
